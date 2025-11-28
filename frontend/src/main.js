fc5073d09e639e2131a2c2f3f2ac45f541be5ece
Ubuntu <kefaslungu@HNG-13.j0eibxrgtxcedl311kzupzdegd.cx.internal.cloudapp.net>
2025-11-28 01:59:50 +0000

    Final working microservices
    Signed-off-by: Ubuntu <kefaslungu@HNG-13.j0eibxrgtxcedl311kzupzdegd.cx.internal.cloudapp.net>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,24 +5,22 @@ import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
-
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
+
+// Configure API base URL for todos and users APIs
+Vue.http.options.root = window.location.protocol + '//' + window.location.host + '/api'
 
 import App from '@/components/App'
 import router from './router'
 import store from './store'
-
 Vue.config.productionTip = false
-
 /* Auth plugin */
 import Auth from './auth'
 Vue.use(Auth)
-
 /* Auth plugin */
 import Zipkin from './zipkin'
 Vue.use(Zipkin)
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
